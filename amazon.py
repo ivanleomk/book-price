76477cf60b8e705999a552c7165d36a96881e4cb
ivanleo97 <ivanleo97@gmail.com>
2019-01-04 20:42:07 +0700

    App V1.4
    Changes
    1. Web Scrapping functionality abstracted out of app.py. it is now imported from amazon.py

---
+++ amazon.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+def amazonData(isbn,key):
+    #STUB Testing
+    base = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords="
+    headers = {'user-agent': key}
+
+    url = base+isbn
+
+    page = requests.get(url, headers=headers)
+    #Converting it to a soup for scrapping
+    soup = BeautifulSoup(page.content, 'html.parser')
+    while(len(soup)==3):
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        print("Failed Attempt")
+        print(len(soup))
+
+    #Grabbing the item listing
+    item = soup.find_all("div",class_="a-fixed-left-grid")
+    #Finds the Title
+    title = soup.find_all("h2",class_="s-access-title")[0].get_text()
+    price = soup.find_all("span",class_="sx-price-large")[0].get_text().split('\n')
+    price = price[1]+price[2]+"."+price[3]
+
+    data = [title,price]
+    #returns Amazon Title and Price
+    return data
--- app.py
+++ app.py
@@ -1,32 +1,16 @@
 #Import
 from bs4 import BeautifulSoup
 import requests
+
+#Abstracted Functions
 from userkey import getkey
+from amazon import amazonData
 
 #Generates a user agent to use for the web scrapping
 key = getkey()
 #Using Request to grab the site
-headers = {'user-agent': key}
-
-#STUB Testing
-base = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords="
 isbn = "9780872208438"
-url = base+isbn
+#Grabs Amazon Prices
+amazondata = amazonData(isbn,key)
 
-page = requests.get(url, headers=headers)
-#Converting it to a soup for scrapping
-soup = BeautifulSoup(page.content, 'html.parser')
-while(len(soup)==3):
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    print("Failed Attempt")
-    print(len(soup))
-
-#Grabbing the item listing
-item = soup.find_all("div",class_="a-fixed-left-grid")
-#Finds the Title
-title = soup.find_all("h2",class_="s-access-title")[0].get_text()
-price = soup.find_all("span",class_="sx-price-large")[0].get_text().split('\n')
-price = price[1]+price[2]+"."+price[3]
-
-print(title+ " is "+price.strip('\n') +" on Amazon")
+print(amazondata[0] +" retails at " + amazondata[1])
