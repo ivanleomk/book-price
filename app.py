758b2118f086a829744b301a1b7b1e98fd3be1dd
ivanleo97 <ivanleo97@gmail.com>
2019-01-04 20:36:12 +0700

    App V1.3
    Changes
    *Dynamically chooses a random user agent key from a huge list of keys from whatismybrowser.com
    *Prices on Amazon displayed in console FINALLY

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 from userkey import getkey
 
+#Generates a user agent to use for the web scrapping
+key = getkey()
+#Using Request to grab the site
+headers = {'user-agent': key}
+
 #STUB Testing
 base = "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords="
 isbn = "9780872208438"
 url = base+isbn
-
-key = getkey()
-#Using Request to grab the site
-headers = {'user-agent': key}
 
 page = requests.get(url, headers=headers)
 #Converting it to a soup for scrapping
@@ -25,7 +26,7 @@ while(len(soup)==3):
 item = soup.find_all("div",class_="a-fixed-left-grid")
 #Finds the Title
 title = soup.find_all("h2",class_="s-access-title")[0].get_text()
-price = soup.find_all("span",class_="sx-price-large")[0].get_text()
+price = soup.find_all("span",class_="sx-price-large")[0].get_text().split('\n')
+price = price[1]+price[2]+"."+price[3]
 
-print(title+ " is "+price)
-#Grabbing the item that fits the ISBN number
+print(title+ " is "+price.strip('\n') +" on Amazon")
--- userkey.py
+++ userkey.py
@@ -6,18 +6,36 @@ import requests
 #This generates a random headers so as to bypass Amazon and other website's
 
 def getkey():
-    #Chooses a random page from the list of chrome browsers
-    pagenum = random.randint(1,4425)
-    url = "https://developers.whatismybrowser.com/useragents/explore/software_name/chrome/" + str(pagenum)+"?order_by=times_seen"
+    #Chooses a random User Agent from the list provided
+    url = "https://developers.whatismybrowser.com/useragents/explore/software_name/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content,'html.parser')
+    providers = soup.find_all('tr')
+
+    #Converts the entire list into a list for easy workaround
+    list = [x.get_text().strip().split("\n") for x in providers[1:]]
+
+    #Chooses a random page from the list of user agents
+    cprovid = list[random.randint(0,len(list))]
+    #counts the number of related agents
+    relatedagents = cprovid[1]
+
+    #Chooses the user agent
+    provider = cprovid[0]
+    #Calculates the number of pages
+    pages = 1+ int(relatedagents)//50
+    pagenum = random.randint(1,int(pages))
+    print("We have chosen %s with %s page(s) and have chosen %s pagenumber"%(provider,pages,pagenum))
+    provider = ("-").join(provider.lower().split(' '))
+    url = "https://developers.whatismybrowser.com/useragents/explore/software_name/" +provider+"/" + str(pagenum)+"?order_by=times_seen"
+    print(url)
     #Logs an entire list of user agent data values
     page = requests.get(url)
-
     #Converts the page content to a BeautifulSoup
     soup = BeautifulSoup(page.content, 'html.parser')
     keys = soup.find_all('td', class_='useragent')
-
+    print("There are %s keys"%(len(keys)))
     #Selects a random entry from the list of keys on the page
-    selected = random.randint(0,49)
-
+    selected = random.randint(0,len(keys)-1)
     #Returns user key
     return(keys[selected].get_text())
